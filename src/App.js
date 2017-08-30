e29495365af97acca463ad9e435b148a9e48baa7
wbdana <william.b.dana@gmail.com>
2017-08-30 18:21:00 -0400

    Implement basic TextEditor component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TextEditor from './components/TextEditor'
+import NavBar from './components/NavBar'
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
 
 class App extends Component {
+  state = {
+    activePage: 'home'
+  }
+
+  updateActivePage = (newPage) => {
+    this.setState({
+      activePage: newPage
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar activePage={this.state.activePage} updateActivePage={this.updateActivePage} />
+        <TextEditor />
       </div>
     );
   }
---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Header, Menu, Icon } from 'semantic-ui-react'
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      activePage: this.props.activePage
+    }
+  }
+
+  handleItemClick = (e, { name }) => {
+    this.setState({ activePage: name})
+    this.props.updateActivePage(name)
+  }
+
+  render() {
+    const { activePage } = this.state
+    return(
+      <div className="NavBar">
+        <Header size="huge">ConnText</Header>
+        <Header size="small">Connected Text</Header>
+        <Menu>
+          <Menu.Item name="home" active={activePage === 'home'} onClick={this.handleItemClick}>
+            <Icon name='home' />Home
+          </Menu.Item>
+          <Menu.Item name='files' active={activePage === 'files'} onClick={this.handleItemClick}>
+            <Icon name='file' />Files
+          </Menu.Item>
+        </Menu>
+      </div>
+    )
+  }
+}
+
+export default NavBar

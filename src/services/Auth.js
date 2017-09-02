cb59c6553d68c367abae59904a1710f4ec09356e
wbdana <william.b.dana@gmail.com>
2017-09-02 19:22:33 -0400

    Begin Auth setup

---
+++ src/services/Auth.js
@@ -0,0 +1,32 @@
+import { APIURL } from '../components/PageAssets'
+
+class Auth {
+  static login(loginParams) {
+    const options = {
+      "method": "POST",
+      "headers": {
+        "content-type": "application/json",
+        "accept": "application/json",
+        "Authorization": localStorage.getItem('jwt')
+      },
+      "body": JSON.stringify(loginParams)
+    }
+    return fetch("http://localhost:3000/user_token", options)
+      .then(resp => resp.json())
+  }
+
+  static currentUser () {
+    const options = {
+      "method": "GET",
+      "headers": {
+        "content-type": "application/json",
+        "accept": "application/json",
+        "Authorization": localStorage.getItem('jwt')
+      }
+    }
+    return fetch("http://localhost:3000/cu", options)
+      .then(resp => resp.json())
+  }
+}
+
+export default Auth

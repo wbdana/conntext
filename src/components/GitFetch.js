02f04fa3dfcb616fb28c4bed2a7e1fd9d0eab85b
wbdana <william.b.dana@gmail.com>
2017-09-07 18:00:21 -0400

    Build GitFetch

--- src/components/GitFetch.js
+++ src/components/GitFetch.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import base64 from 'base-64'
 import { Container, Input, Button } from 'semantic-ui-react'
-import { APIURL } from './PageAssets'
+import { APIURL, TSP } from './PageAssets'
+import { GHTK } from '../.secret.js'
 
 class GitFetch extends React.Component {
   state = {
@@ -27,12 +29,41 @@ class GitFetch extends React.Component {
     })
   }
 
+  fetchFile = () => {
+    console.log(this.state)
+    console.log(`https://api.github.com/repos/${this.state.githubUsername}/${this.state.repoName}/contents/${this.state.filePath}`)
+    const options = {
+      "method": "get",
+      "headers": {
+        "Authorization": `${GHTK}`,
+        "content-type": "application/json",
+        "accept": "application/vnd.github.v3+json"
+      }
+    }
+    const newOptions = {
+      "method": "post",
+      "headers": {
+        "content-type": "application/json",
+        "accept": "application/json"
+      },
+
+    }
+    fetch(`https://api.github.com/repos/${this.state.githubUsername}/${this.state.repoName}/contents/${this.state.filePath}`, options)
+      .then(resp => resp.json())
+      .then(json => console.log(base64.decode(json.content.replace(/^\s+|\s+$/gm, '').split('\n').join(''))))
+  }
+
   render() {
     return(
       <Container className="gitFetch">
+        <TSP />
         <Input label='GitHub Username' type='text' onChange={this.updateGithubUsername} />
+        <TSP />
         <Input label='Repo Name' type='text' onChange={this.updateRepoName} />
+        <TSP />
         <Input label='Full file path (with extension)' type='text' onChange={this.updateFilePath} />
+        <TSP />
+        <Button onClick={this.fetchFile}>GitFetch!</Button>
       </Container>
     )
   }

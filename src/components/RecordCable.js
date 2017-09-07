01cc9ab39c7533fa47750ef1e7ba86a5d9a40f3e
wbdana <william.b.dana@gmail.com>
2017-09-07 14:50:13 -0400

    Remove console.logs

--- src/components/RecordCable.js
+++ src/components/RecordCable.js
@@ -4,12 +4,9 @@ class RecordCable extends React.Component {
 
 
   componentDidMount() {
-    console.log(this.props['data-recordId'])
-    console.log(window.location.href.match(/\d+$/))
     if (window.location.href.match(/\d+$/) !== null) {
       this.props['data-cableApp'].record = this.props['data-cableApp'].cable.subscriptions.create({channel: "RecordChannel", room: window.location.href.match(/\d+$/)[0]}, {
         received: (data) => {
-          console.log(data)
           this.props.updateWSContent(data)
         }
       })

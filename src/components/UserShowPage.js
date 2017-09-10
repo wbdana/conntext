20ad15e290c73b5ecf2edfb0aa54182063b72158
wbdana <william.b.dana@gmail.com>
2017-09-10 15:43:04 -0400

    Remove getFileExtension() from UserShowPage

--- src/components/UserShowPage.js
+++ src/components/UserShowPage.js
@@ -14,29 +14,6 @@ class UserShowPage extends React.Component {
     this.props.resetViewUser()
   }
 
-  getFileExtension = (file) => {
-    switch (file.language) {
-      case 'ruby':
-        return '.rb'
-      case 'javascript':
-        return '.js'
-      case 'python':
-        return '.py'
-      case 'csharp':
-        return '.cs'
-      case 'xml':
-        return '.xml'
-      case 'markdown':
-        return '.md'
-      case 'css':
-        return '.css'
-      case 'html':
-        return '.html'
-      default:
-        return '.rb'
-    }
-  }
-
   render() {
     return(
       <div className="userShowPage">
@@ -73,7 +50,7 @@ class UserShowPage extends React.Component {
                       <List.Item key={index}>
                         <List.Icon name='github' size='large' verticalAlign='middle' />
                         <List.Content>
-                          <List.Header  id={file.id}>{`${file.name}${this.getFileExtension(file)}`}</List.Header>
+                          <List.Header  id={file.id}>{file.name}</List.Header>
                           <List.Description>Last updated {file.updated_at}</List.Description>
                         </List.Content>
                       </List.Item>

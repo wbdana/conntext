65d9f1527ca99d343e21b0bc663f09f5f341102d
wbdana <william.b.dana@gmail.com>
2017-09-09 20:36:39 -0400

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -32,8 +32,20 @@ const About = () => {
 
       <Header size="small">Current Recommended Usage of Connected Text</Header>
       <ul>
+        <li>Editor: Use New to create a new file, or grab one off of GitHub with GitFetch (see below). You can access the file again from Home or your My Files page.</li>
+        <li>Sharing files: From a file's Editor page, you can add a collaborator to that file by entering their username (must be exact) in the appropriate field. Shared files will appear in your collaborator's Shared Files page.</li>
         <li>File naming conventions: Currently, Connected Text does not support repository/folder structures for your saved files. As such, we recommend that you add your full file path to your file name. For example, instead of 'Login', use 'repo_name/src/components/Login.js'.</li>
         <li>Syntax highlighting: Simply select the language you need to enable syntax highlighting in the Editor.</li>
+        <li>GitFetch: Use GitFetch to grab files directly from GitHub. Simply enter the repository owner's username, the name of the repo, and the full file path (including extension) of the repo. For example:
+          <ul>
+            <li>Username: wbdana</li>
+            <li>Repo Name: conntext</li>
+            <li>File Path: src/App.js</li>
+          </ul>
+        Files created using GitFetch are automatically saved to your account.
+        </li>
+        <li>Saving files: Once you have created a file through New or GitFetch, updates to that file will be saved automatically. </li>
+        <li>File chat: Each file has its own chatroom associated with it. You can view messages about a file on the Editor page for that file.</li>
       </ul>
     </Container>
   )

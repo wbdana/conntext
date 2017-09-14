9f7d517c680c38df735db1ea5f6a5c1b1d00b7df
wbdana <william.b.dana@gmail.com>
2017-09-13 22:36:58 -0400

    Add scroll to bottom in Messages

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,29 +1,46 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { Container, List } from 'semantic-ui-react'
 import ScrollArea from 'react-scrollbar'
 
-const Messages = (props) => {
+class Messages extends React.Component {
 
-  return(
-    <Container className="messages">
-      <ScrollArea speed={0.8} className="area" contentClassName="content" horizontal={false}>
-        <List celled animated>
-          {props.messages.map( (message, index) => {
-            return(
-              <List.Item key={index}>
-                <List.Description>
-                  {message.username}: {message.content}
-                </List.Description>
-              </List.Item>
-            )
-          })}
-          <List.Item>
-            <div style={{float:"left", clear:"both"}} ref={(el) => {this.messagesEnd = el;}} />
-          </List.Item>
-        </List>
-      </ScrollArea>
-    </Container>
-  )
+  scrollToBottom = () => {
+    const node = ReactDOM.findDOMNode(this.messagesEnd)
+    node.scrollIntoView({ behavior: "smooth" })
+  }
+
+  componentDidMount(){
+    this.scrollToBottom()
+    console.log('Should have worked!')
+  }
+
+  componentDidUpdate(){
+    this.scrollToBottom()
+  }
+
+  render() {
+    return(
+      <Container className="messages">
+        <ScrollArea speed={0.8} className="area" contentClassName="content" horizontal={false} onChange={this.handleChange}>
+          <List celled animated>
+            {this.props.messages.map( (message, index) => {
+              return(
+                <List.Item key={index}>
+                  <List.Description>
+                    {message.username}: {message.content}
+                  </List.Description>
+                </List.Item>
+              )
+            })}
+            <List.Item>
+              <div style={{float:"left", clear:"both"}} ref={(el) => {this.messagesEnd = el;}} />
+            </List.Item>
+          </List>
+        </ScrollArea>
+      </Container>
+    )
+  }
 }
 
 export default Messages
